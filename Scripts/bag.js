46847df470ccc261cb675182098a47f3301fc6c6
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-12-04 20:27:14 +0530

    Final Commit

--- Scripts/bag.js
+++ Scripts/bag.js
@@ -1,9 +1,55 @@
+const CONVENIENCE_FEE = 99;
+
 let bagItemObjects;
 
 onLoad();
 function onLoad() {
   loadBagItemObjects();
   displayBagitems();
+  displayBagSummary();
+}
+
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector(".bag-summary");
+
+  let totalItem = bagItemObjects.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+  let finalPayment = 0;
+
+  bagItemObjects.forEach((bagItem) => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+
+  finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEE;
+  bagSummaryElement.innerHTML = `
+  
+    <div class="bag-details-container">
+                    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+                    <div class="price-item">
+                        <span class="price-item-tag">Total MRP</span>
+                        <span class="price-item-value">₹${totalMRP}</span>
+                    </div>
+                    <div class="price-item">
+                        <span class="price-item-tag">Discount on MRP</span>
+                        <span class="price-item-value priceDetail-base-discount">-₹ ${totalDiscount}</span>
+                    </div>
+                    <div class="price-item">
+                        <span class="price-item-tag">Convenience Fee</span>
+                        <span class="price-item-value">₹ 99</span>
+                    </div>
+                    <hr>
+                    <div class="price-footer">
+                        <span class="price-item-tag">Total Amount</span>
+                        <span class="price-item-value">₹ ${finalPayment}</span>
+                    </div>
+                </div>
+                <button class="btn-place-order">
+                    <div class="css-xjhrni">PLACE ORDER</div>
+                </button>
+
+  `;
 }
 
 function loadBagItemObjects() {
@@ -33,6 +79,7 @@ function removeFromBag(itemId) {
   loadBagItemObjects();
   displayBagIcon();
   displayBagitems();
+  displayBagSummary();
 }
 
 function generateItemHTML(item) {
